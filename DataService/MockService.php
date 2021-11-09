30e897ccc70354f85b9bf1b6d96978845ca7de7f
kamiljam <kamiljam580@gmail.com>
2021-11-09 13:16:15 +0100

    Added

--- DataService/MockService.php
+++ DataService/MockService.php
@@ -3,9 +3,26 @@
 include_once("IDataService.php");
 class MockService implements IDataService
 {
+    public function Connect($serverName, $Database, $user, $passwd)
+    {
 
 
-    
+    }
+
+    public function ReadAll()
+    {
+
+    }
+    public function Add($name, $defeated, $cd)
+    {
+
+    }
+    public function Remove($id)
+    {
+        
+    }
+
+
 }
 
 
